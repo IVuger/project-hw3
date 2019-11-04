54be0381cc01f9c11f0cef1dea1681c832db49b9
IVuger <ivan.vuger.online@gmail.com>
2019-11-05 00:39:45 +0100

    upload

---
+++ MDK/Boards/ST/STM32F429I-Discovery/Blinky/Debug/blinky.d
@@ -0,0 +1,6 @@
+.\debug\blinky.o: Blinky.c
+.\debug\blinky.o: C:\Users\Ivan\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\RTOS2\Include\cmsis_os2.h
+.\debug\blinky.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\debug\blinky.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+.\debug\blinky.o: C:\Users\Ivan\AppData\Local\Arm\Packs\Keil\MDK-Middleware\7.9.0\Board\Board_LED.h
+.\debug\blinky.o: C:\Users\Ivan\AppData\Local\Arm\Packs\Keil\MDK-Middleware\7.9.0\Board\Board_Buttons.h
